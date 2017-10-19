19df4be82c0e61f53f7fc78dddd35ea272ebf613
mazhar <p14126085@myemail.dmu.ac.uk>
2017-10-19 16:43:21 +0100

    java header

--- js/greeting.js
+++ js/greeting.js
@@ -3,9 +3,11 @@
   let myName = document.getElementById("my-name"); 
   let userName = document.getElementById("user-name"); 
   let getName = document.getElementById("get-name");
-
+  let userHeader = document.getElementById("myHeader");
+  
   function PerformGreeting(ev) {
     myName.innerHTML = userName.value;
+	userHeader.innerHTML = userName.value;
     ev.preventDefault();
   }
 
